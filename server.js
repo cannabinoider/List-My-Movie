0b6e436077c627d009b93861af8f1e28468492d9
Shubham-Yadav14 <shubhamrao14102002@gmail.com>
2024-05-21 00:03:41 +0530

    no change

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser')
 const app = express();
-const port=6969;
+const port = 6969;
 
 app.get('/', function (req, res) {
- return res.send('Hello');
+    return res.send('Hello');
 });
 
-app.listen(port, () => { console.log(`Server is running on port ${port}`); });
+app.listen(port, () => {
+    console.log(`Server is running: http://localhost:${port}`);
+});
